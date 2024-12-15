4ffa206a9d24f209c441174a227092627f764d25
Ilya Zheman <zheman@yohoho.games>
2024-12-15 17:51:34 +0300

    Спавн предметов

---
+++ Assets/Scripts/Game/Data/ItemsDataStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Data
+{
+    [CreateAssetMenu(fileName = "ds_items", menuName = "Data/Items", order = 0)]
+    public class ItemsDataStorage : ScriptableObject
+    {
+        [SerializeField] private ItemData[] items;
+        public ItemData[] Items => items;
+    }
+
+    [Serializable]
+    public class ItemData
+    {
+        [SerializeField] private Color color;
+        public Color Color => color;
+    }
+}
---
+++ Assets/Scripts/Game/Logic/ItemsFactory.cs
@@ -0,0 +1,28 @@
+﻿using Game.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Logic
+{
+    public class ItemsFactory : MonoBehaviour
+    {
+        [SerializeField] private ItemsDataStorage itemsDataStorage;
+        [SerializeField] private ScrollRect scrollRect;
+        [SerializeField] private BaseDraggableItem draggableItemPrefab;
+        [SerializeField] private Transform itemsContainer;
+
+        private void Awake()
+        {
+            GenerateItems();
+        }
+
+        private void GenerateItems()
+        {
+            foreach (var itemData in itemsDataStorage.Items)
+            {
+                var item = Instantiate(draggableItemPrefab, itemsContainer, false);
+                item.Init(scrollRect, itemData.Color);
+            }
+        }
+    }
+}
